c928bc05e9fa09b8fa6b34c552a938cb0b590b99
Shivam-20011767F <102542012+Shivam-20011767F@users.noreply.github.com>
2022-04-04 16:44:14 +0530

    Update Unit1_P2_Task1.java

--- Practical2/Unit1_P2_Task1.java
+++ Practical2/Unit1_P2_Task1.java
@@ -7,7 +7,7 @@ public class Unit1_P2_Task1 {
     {
         int float=2.3; //float can not be variable name
         float int=2; //int can not be variable name
-        int char ="Prashant";//char can not be variable name
+        int char ="Shivam";//char can not be variable name
 
         System.out.println();
         Student int=new Student;//int can not be variable name
